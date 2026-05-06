39dc6a65935e26968d402702452e0e6e0752c8c1
Diego Castro Satt <diegoa.castro@pregrado.uoh.cl>
2026-05-06 07:36:57 -0400

    delete Rutina.cs por ambiguedad

--- Models/Rutina.cs
+++
@@ -1,8 +0,0 @@
-namespace Softawer.Models;
-
-public class Rutina
-{
-    public int Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-    public string Descripcion { get; set; } = string.Empty;
-}
